bdf042ec66c39f9a3275ddca57d1e5c6b996b89a
Roman Tatarskikh <sentpim@gmail.com>
2016-11-15 13:45:37 +0300

    + frontend

---
+++ Users/sentpim/Desktop/HotelManagmentInno-master/HotelManagmentInno-master/src/main/java/zakirskikh/controller/PersonController.java
@@ -0,0 +1,19 @@
+package zakirskikh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Anvar on 11/11/2016.
+ */
+@Controller
+public class PersonController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String main(){
+        System.out.println(1);
+        return "dashboard";
+    }
+
+}
